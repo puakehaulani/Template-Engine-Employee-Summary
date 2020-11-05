51482460b497ef39cb053e63ac640e54d6455241
puakehaulani <puakehaulani@gmail.com>
2020-11-04 22:46:14 -0800

    output dir

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+if (!fs.existsSync(OUTPUT_DUR)) {
+    fs.mkdirSync(OUTPUT_DUR);
+}
 const render = require("./lib/htmlRenderer");
 
 
@@ -21,8 +24,7 @@ const render = require("./lib/htmlRenderer");
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
